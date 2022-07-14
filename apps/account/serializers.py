8412e7872b8f8a81c3c0eb21120757c19ce3a14c
Enzo Falcón <falcone134@gmail.com>
2022-07-14 17:33:45 -0500

    Refactoring

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -5,22 +5,16 @@ from rest_framework.authtoken.models import Token
 
 from account.models import Profile
 
-class ProfileSerializer(serializers.ModelSerializer):
-
-    class Meta:
-
-        model = Profile
-        exclude = ('user', )
-
 class UserProfileSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(required=True)
-    profile = ProfileSerializer()
+    picture = serializers.ImageField(source="profile.picture", required=False)
+    about_me = serializers.CharField(source="profile.about_me", required=False)
 
     class Meta:
 
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'profile')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'picture', 'about_me')
 
     def validate_email(self, value=None):
 
@@ -30,8 +24,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
-        # Removing profile dict from main json
-        profile = validated_data.pop('profile')
+        # Removing profile fields dict from main json
+        profile = {}
+        if validated_data.get('picture'):
+            profile['picture'] = validated_data.pop('picture')
+        
+        if validated_data.get('about_me'):
+            profile['about_me'] = validated_data.pop('about_me')
 
         user = User.objects.create_user(**validated_data)
         user.is_active = False
