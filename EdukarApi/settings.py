398b737a106498bf785d2f1a818f6aafc99a2f52
Enzo Falcón <falcone134@gmail.com>
2022-08-07 02:21:28 -0500

    Changing user_create serializer

--- EdukarApi/settings.py
+++ EdukarApi/settings.py
@@ -77,7 +77,9 @@ DJOSER = {
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
-    'SERIALIZERS': {},
+    'SERIALIZERS': {
+        'user_create': 'account.serializers.UserProfileSerializer'
+    },
 }
 
 MIDDLEWARE = [
