3cb871aaa80605da6d5eca78ca2b346a6c98a3a9
Enzo Falcon <falcone134@gmail.com>
2022-09-09 00:20:49 -0500

    Fixing setsections default values

--- apps/forum/management/commands/setsections.py
+++ apps/forum/management/commands/setsections.py
@@ -20,19 +20,23 @@ class Command(BaseCommand):
                     section_obj = Section(name=section_name)
                     section_obj.save()
 
+                except IntegrityError:
+                    # If for some reason the section already exists, then we catch
+                    # the error
+                    section_obj = Section.objects.get(name=section_name)
+                    self.stdout.write("{0} section already exists".format(section_name))
+
+                finally:
                     # In this case the section has subsection, so we create those
                     # subsections and sync to the section.
-                    for subsection in section.values():
+                    subsections = list(section.values())[0]
+                    for subsection in subsections:
                         try:
                             subsection_obj = Subsection(section=section_obj, name=subsection)
                             subsection_obj.save()
                         except IntegrityError:
                             # Raise error in case subsection already exists.
                             self.stdout.write("{0} subsection already exists".format(subsection))
-                except IntegrityError:
-                    # If for some reason the section already exists, then we catch
-                    # the error
-                    self.stdout.write("{0} section already exists".format(section_name))
 
         except Section.DoesNotExist:
             # Raise error when Section model has not been created.
