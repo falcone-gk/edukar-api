ab0e53ba1631f63f961a61e005bac61d47565f11
Katthy Crosby Aponte <70598355+KatthyCrosby@users.noreply.github.com>
2022-10-11 12:19:08 -0500

    Add str method to notif types

--- apps/notification/models.py
+++ apps/notification/models.py
@@ -11,6 +11,9 @@ class NotificationTypes(models.Model):
     type_notif = models.CharField(max_length=15, unique=True)
     description = models.CharField(max_length=60, unique=True)
 
+    def __str__(self):
+        return self.type_notif
+
 class Notification(models.Model):
 
     user = models.ForeignKey(User, related_name='notif', on_delete=models.CASCADE)
