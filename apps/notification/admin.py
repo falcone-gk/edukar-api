714fe9f2503f2ceabcefb824ff89da48dfb0c2fd
Katthy Crosby Aponte <70598355+KatthyCrosby@users.noreply.github.com>
2022-10-11 12:18:51 -0500

    Add models to admin

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from notification.models import Notification, NotificationTypes
 
 # Register your models here.
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sender', 'notif_type', 'source_id', 'date')
+
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(NotificationTypes)
