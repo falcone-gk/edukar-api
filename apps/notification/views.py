b9501a34e52d87be4a6f28ccf23c4e1f296b6149
Katthy Crosby Aponte <70598355+KatthyCrosby@users.noreply.github.com>
2022-11-23 05:12:12 -0500

    Last updates

--- apps/notification/views.py
+++ apps/notification/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+
+from forum.paginators import PostCoursePagination
+from notification.models import Notification
+from notification.serializers import NotificationReceiverSerializer, NotificationSenderSerializer
+from notification.permissions import isOwnNotification
 
 # Create your views here.
+
+class NotificationReceivedAPIView(
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet):
+
+    serializer_class = NotificationReceiverSerializer
+    permission_classes = (IsAuthenticated, isOwnNotification)
+    pagination_class = PostCoursePagination
+
+    def get_queryset(self):
+
+        current_user = self.request.user
+        return Notification.objects.filter(user=current_user).order_by('-date')
+
+class NotificationSentAPIView(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet):
+
+    serializer_class = NotificationSenderSerializer
+    permission_classes = (IsAuthenticated, isOwnNotification)
+    pagination_class = PostCoursePagination
+
+    def get_queryset(self):
+
+        current_user = self.request.user
+        return Notification.objects.filter(user=current_user).order_by('-date')
