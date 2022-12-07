b455768813bc22cbb942e80128cb41867abb91c1
Katthy Crosby Aponte <70598355+KatthyCrosby@users.noreply.github.com>
2022-12-06 22:40:36 -0500

    Delete unused apis

--- apps/account/views.py
+++ apps/account/views.py
@@ -12,26 +12,6 @@ from account.serializers import UserProfileSerializer, MyTokenObtainPairSerializ
 
 # Create your views here.
 
-class CreateUserAPIView(generics.CreateAPIView):
-
-    serializer_class = UserProfileSerializer
-
-    def create(self, request, *args, **kwargs):
-
-        # Getting response to override 201 status code
-        response = super(CreateUserAPIView, self).create(request, *args, **kwargs)
-
-        # Returning the same response when error happens
-        if response.status_code == 400:
-            return response
-
-        # Sending a success message instead of object (default).
-        msg = {'success': 'Cuentra creada correctamente!'}
-        return Response(msg, status=status.HTTP_201_CREATED)
-
-class LoginAPIView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
-
 class OwnerPostAPIView(
     mixins.ListModelMixin,
     mixins.UpdateModelMixin,
