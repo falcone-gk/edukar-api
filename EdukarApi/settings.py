162f91bf4b9c5ec1cc629ae6685900f2ab9c7873
Katthy Crosby Aponte <70598355+KatthyCrosby@users.noreply.github.com>
2022-12-12 02:20:33 -0500

    Final config that works

--- EdukarApi/settings.py
+++ EdukarApi/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-CORS_ALLOWED_ORIGINS = ["http://{}".format(env('DOMAIN')), ]
+CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS').split(" ")
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
