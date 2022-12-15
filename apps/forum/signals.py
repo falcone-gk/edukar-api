51aa55df744b31057de81bee52740134e813aaef
Enzo Falcón <falcone134>
2022-12-14 19:41:51 -0500

    Fix signals

--- apps/forum/signals.py
+++ apps/forum/signals.py
@@ -8,8 +8,11 @@ from django.utils.text import slugify
 from notification.models import NotificationTypes, Notification
 from forum.models import Post, Section, Subsection, Comment, Reply
 
-@receiver(pre_save, sender=Post)
-def create_post_title_slug(sender, instance, **kwargs):
+@receiver(post_save, sender=Post)
+def create_post_title_slug(sender, created, instance, **kwargs):
+
+    if not created:
+        return
 
     # Creates a random suffix for slug post title.
     generated_slug = slugify(instance.title)
@@ -21,6 +24,7 @@ def create_post_title_slug(sender, instance, **kwargs):
     generated_slug += '-%s' % random_suffix
 
     instance.slug = generated_slug
+    instance.save()
 
 @receiver(pre_save, sender=Section)
 def create_section_slug(sender, instance, **kwargs):
