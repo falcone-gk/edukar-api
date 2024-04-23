a9bec73b54adfc9c0dd6964587df676dfda172e5
Enzo Falcon <falcone134@gmail.com>
2024-04-23 00:46:00 -0500

    move custom obtain auth token

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,19 +1,2 @@
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
+from django.db import models
 
-class CustomAuthToken(ObtainAuthToken):
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'username': user.username,
-            'email': user.email,
-            'picture': user.profile.get().picture.url,
-            'has_notification': user.notif.filter(is_read=False).exists()
-        })
