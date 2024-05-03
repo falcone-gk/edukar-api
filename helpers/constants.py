397080f9648546171864cfafcbc05d5cd6cf0e9f
Enzo Falcon <falcone134@gmail.com>
2024-05-03 01:10:02 -0500

    add helpers for forum module

---
+++ helpers/constants.py
@@ -0,0 +1,2 @@
+# first params in section slug and second is post slug
+POST_PATH = '/forum/{}/posts/{}'
---
+++ helpers/messages.py
@@ -0,0 +1,7 @@
+class CommentForumNotification:
+    TITLE = 'Comentaron tu post'
+    DESCRIPTION = 'El usuario {} comentó en tu post: {}'
+
+class ReplyForumNotification:
+    TITLE = 'Respondieron a tu comentario'
+    DESCRIPTION = 'El usuario {} te respondió en el post: {}'
