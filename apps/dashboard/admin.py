b005bd42a9fc3e20c447fcc074e51d888a208c95
Enzo Falcon <falcone134@gmail.com>
2024-12-06 10:47:27 -0500

    Add dashboard model to admin

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,3 +1,11 @@
+from dashboard.models import DownloadExams
 from django.contrib import admin
 
 # Register your models here.
+
+
+class DownloadExamAdmin(admin.ModelAdmin):
+    list_display = ("user", "exam", "downloaded_at", "download_successful")
+
+
+admin.site.register(DownloadExams, DownloadExamAdmin)
