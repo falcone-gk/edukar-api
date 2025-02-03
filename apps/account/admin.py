a9b73f544daa9f5b044f9674d5ea327dee5ba228
Enzo Falcon <falcone134@gmail.com>
2025-02-03 07:25:59 -0500

    add userpod relation to admin

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,6 +1,7 @@
+from account.models import Profile, UserProduct
 from django.contrib import admin
-from account.models import Profile
 
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(UserProduct)
