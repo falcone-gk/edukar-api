bf0151ce5d8048743138e3871e68c04ab96791d1
Enzo Falcon <falcone134@gmail.com>
2025-02-15 14:16:23 -0500

    Add admin inlines

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -13,15 +13,26 @@ from store.models import (
 # Register your models here.
 
 
+class ProductAttributeInline(admin.TabularInline):
+    model = ProductAttribute
+    extra = 1
+
+
+class VideoPartInline(admin.TabularInline):
+    model = VideoPart
+    extra = 1
+
+
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "price", "type")
+    inlines = [ProductAttributeInline, VideoPartInline]
 
 
 admin.site.register(Product, ProductsAdmin)
 admin.site.register(Category)
 admin.site.register(Attribute)
 admin.site.register(AttributeOption)
-admin.site.register(ProductAttribute)
-admin.site.register(VideoPart)
+# admin.site.register(ProductAttribute)
+# admin.site.register(VideoPart)
 admin.site.register(Sell)
 admin.site.register(Claim)
