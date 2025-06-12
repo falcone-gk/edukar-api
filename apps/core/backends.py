2e64fd7af074b0a8591c40c37c86c71cbf1f7a2a
Enzo Falcon <falcone134@gmail.com>
2025-06-12 01:37:43 -0500

    fix username validation

--- apps/core/backends.py
+++ apps/core/backends.py
@@ -1,25 +1,22 @@
-from django.conf import settings
-from django.contrib import auth
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 
+
 class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None):
+        user_model = get_user_model()
 
-	def authenticate(self, request, username=None, password=None):
+        try:
+            user = user_model.objects.get(
+                Q(username__exact=username) | Q(email__iexact=username)
+            )
 
-		user_model = get_user_model()
+            if not user.is_active:
+                return None
 
-		try:
-			user = user_model.objects.get(
-				Q(username__iexact=username) | Q(email__iexact=username)
-			)
+            if user.check_password(password):
+                return user
 
-			if not user.is_active:
-				return None
-
-			if user.check_password(password):
-				return user
-
-		except user_model.DoesNotExist:
-			return None
+        except user_model.DoesNotExist:
+            return None
